01365d6f4c93364e25cc8425cb7cd789a6225198
ochilovich09-web <ochilovich09@gmail.com>
2026-09-03 22:46:24 +0500

    Remove error logging text from db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,8 +3,7 @@ import mongoose from 'mongoose'
 export async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGO_URI)
-  } catch (err) {
-    console.error(err.message)
+  } catch {
     process.exit(1)
   }
 }
