feb3629999f984491361606393fff4a213f72e78
ochilovich09-web <ochilovich09@gmail.com>
2026-09-03 22:42:22 +0500

    Simplify db connection logging

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,10 @@
 import mongoose from 'mongoose'
 
 export async function connectDB() {
-  const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/ozodcoder'
   try {
-    await mongoose.connect(uri)
-    console.log('MongoDB ulandi')
+    await mongoose.connect(process.env.MONGO_URI)
   } catch (err) {
-    console.error('MongoDB ulanishda xatolik:', err.message)
+    console.error(err.message)
     process.exit(1)
   }
 }
